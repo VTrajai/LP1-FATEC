85b0b6d52723159a5304653c451ac4b569dc4d75
VTrajai <vitortrjgn@gmail.com>
2026-03-03 22:48:03 -0300

    Add files via upload

---
+++ Atividade 6/atv4A/src/main/java/com/mycompany/atv4a/Produtos.java
@@ -0,0 +1,63 @@
+public class Produtos {
+   
+    public String nome;
+    public double preco;
+    public String categoria;
+    //metodos
+    //AplicarDesconto
+    //AtualizarPreço
+    //ObterDescriçãoCurta
+
+//Produtos p1 = new Produtos("Chiclete",3, 50, "Alimento");
+//Produtos p2 = new Produtos ("Aspirina", 10, 50, "Medicamento");
+//Produtos p3 = new Produtos ("Shampoo", 22, "Cosmético");
+public class Produto (String nome, double preco, String categoria) {
+    this.nome = nome;
+    this.preco = preco;
+    this.categoria = categoria
+            }
+}
+th
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
---
+++ Atividade 6/atv4A/src/main/java/com/mycompany/atv4a/Venda.java
@@ -0,0 +1,9 @@
+public class Venda {
+    private List<Produto> itens;
+    private double valorTotal;
+    private String formaPagamento;
+    //metodos
+    //AdicionarItem
+    //CalcularTotal
+    //ConfirmarPagamento
+}
